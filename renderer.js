894da6e4e77c059df54590bb792c80672a96a738
MCF-Redfield <37851964+MCF-Redfield@users.noreply.github.com>
2019-05-06 07:32:51 -0300

    Ajuste interface

--- renderer.js
+++ renderer.js
@@ -1,28 +1,28 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var read = require('read-file-utf8')
 var loki = require('lokijs');// instanciando o Banco de dados
-var db = new loki('db.json');//Criando o banco de dados
+var db = new loki('db.json');//Criando o banco de dados 
+var data = read(__dirname + '/db.json');
+db.loadJSON(data);
+
 var clientes = db.getCollection('clientes');
-if (!clientes)
-{
+if (!clientes) {
     console.log('Tabela criada');
     clientes = db.addCollection('clientes');
-}else
-    {
-        console.log("Tabela existe");
-    }
-clientes.insert([{
-    nome: 'Andrea Pirlo',
-    dataNasc: '1981-10-17',
-    cpf: '123456789-10',
-    telefone: '64999531673',
-    endereco: 'Rua 06 Qd13 Lt23',
-    numero: '23',
-    bairro: 'Nadin Saud',
-    cidade: 'Pires do Rio',
-    estado: 'GO'
-    },{
+
+    clientes.insert([{
+        nome: 'Andrea Pirlo',
+        dataNasc: '1981-10-17',
+        cpf: '123456789-10',
+        telefone: '64999531673',
+        endereco: 'Rua 06 Qd13 Lt23',
+        numero: '23',
+        bairro: 'Nadin Saud',
+        cidade: 'Pires do Rio',
+        estado: 'GO'
+    }, {
         nome: 'Karin Benzema',
         dataNasc: '1984-05-27',
         cpf: '123456789-10',
@@ -32,9 +32,13 @@ clientes.insert([{
         bairro: 'Bairro Cramberry',
         cidade: 'Pires do Rio',
         estado: 'GO'
-        }]
+    }]
     );
-db.save();
+    db.save();
+
+} else {
+    console.log("Tabela existe");
+}
 function ready(fn) {
     if (document.readyState != 'loading') {
         console.log("Não carregada")
@@ -64,7 +68,7 @@ ready(function ()/*Será executada qndo a página estiver carregada*/ {
         clientes.insert(cliente);
         console.log("Inserido");
 
-        db.saveDatabase();
+        db.save();
         console.log("Salvo");
     })
 })
