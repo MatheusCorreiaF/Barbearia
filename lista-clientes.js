b0ccd9e74cd65ee6648ed043c17d9f9f2670cd51
MCF-Redfield <37851964+MCF-Redfield@users.noreply.github.com>
2019-05-13 07:39:17 -0300

    Interface
    -add Modals

---
+++ lista-clientes.js
@@ -0,0 +1,55 @@
+// You can also require other files to run in this process
+//require('./renderer.js')
+var read = require('read-file-utf8')
+var loki = require('lokijs');// instanciando o Banco de dados
+var db = new loki('db.json');//Criando o banco de dados 
+var data = read(__dirname + '/db.json');
+db.loadJSON(data);
+//console.log(db);
+
+window.Vue = require('vue');
+
+var clientes = db.getCollection('clientes');
+if (!clientes) {
+    clientes = db.addCollection('clientes');
+}
+
+new Vue({
+    el: 'body',/*onde iremos trabalhar com o Vue*/
+    data: {/*local para declarar variáveis*/
+        clientes: [],/*array para receber clientes do banco de dados*/
+        cliente: {/*objeto Cliente com valores default*/
+            nome: '',
+            dataNasc: '',
+            cpf: '',
+            telefone: '',
+            endereco: '',
+            numero: '',
+            bairro: '',
+            cidade: '',
+            estado: ''
+        },
+        openModal: false
+    },
+    ready: function ()/*função executada assim que carrega página*/ {
+        this.clientes = clientes.data;/*carregando lista de clientes do banco de dados*/
+        console.log(this.clientes);
+    },
+    methods:/*local para declarar métodos*/
+    {
+        abrirModalEditarCliente: function (clienteEditing) {
+            this.openModal = true;
+            this.cliente = clienteEditing;/*Objeto default recebe infos do Cliente a ser editado*/
+        },
+
+        salvarEdicaoCliente: function () {
+            clientes.update(this.cliente);
+            db.save();
+            this.openModal = false;
+        },
+        excluirCliente: function (clienteRemoving) {
+            clientes.remove(clienteRemoving);
+            db.save();
+        }
+    },
+})
